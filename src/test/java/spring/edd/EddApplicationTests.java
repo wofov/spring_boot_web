9520d573d35858bb328889e7da2d0ec25f309ac3
wofov <hys950526@naver.com>
2024-02-23 15:58:12 +0900

    basic

---
+++ src/test/java/spring/edd/EddApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.edd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
