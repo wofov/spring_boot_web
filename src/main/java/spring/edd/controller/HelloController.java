0564cf898aa44869463e2273a90e65e817e62398
wofov <hys950526@naver.com>
2024-02-24 10:51:52 +0900

    객체 반환

--- src/main/java/spring/edd/controller/HelloController.java
+++ src/main/java/spring/edd/controller/HelloController.java
@@ -4,18 +4,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(Model model){
+    public String hello(Model model) {
         System.out.println("-");
-        model.addAttribute("data","hi");
+        model.addAttribute("data", "hi");
         return "hello.html";
     }
+
     @GetMapping("/hi")
-    public String mvc(@RequestParam(required = false) String name, Model model){
-        model.addAttribute("name",name);
+    public String mvc(@RequestParam(required = false) String name, Model model) {
+        model.addAttribute("name", name);
         return "tem";
     }
+
+    @GetMapping("string")
+    @ResponseBody
+    public String helloString(@RequestParam String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+
 }
