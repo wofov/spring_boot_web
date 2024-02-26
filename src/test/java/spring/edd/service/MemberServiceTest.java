71e750e671f5c12fb5041e6605188a40ee40d59e
wofov <hys950526@naver.com>
2024-02-26 15:36:59 +0900

    sevice 테스트 코드 작성

---
+++ src/test/java/spring/edd/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package spring.edd.service;
+
+import org.assertj.core.api.AbstractStringAssert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import spring.edd.domain.Member;
+import spring.edd.repository.MemoryMemberRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+    MemberService memberService;
+    MemoryMemberRepository repository;
+    @BeforeEach
+    public void beforeEach(){
+        repository = new MemoryMemberRepository();
+        memberService = new MemberService(repository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+
+    }
+
+    @Test
+    void validateDuplicateMember() {
+        Member member = new Member();
+        member.setName("sp");
+
+        Member member1 = new Member();
+        member1.setName("sp");
+
+        memberService.join(member);
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member1));
+        assertThat(e.getMessage()).isEqualTo("exist user");
+
+//        try{
+//            memberService.join(member1);
+//            fail();
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("exist user1");
+//        }
+    }
+
+    @Test
+    void findOne() {
+    }
+}
