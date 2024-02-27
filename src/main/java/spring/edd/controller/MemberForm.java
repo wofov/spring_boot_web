070a4995fd9d40be5b58530f9c65ea3d41cbe5a0
wofov <hys950526@naver.com>
2024-02-27 16:23:41 +0900

    form 태그

---
+++ src/main/java/spring/edd/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package spring.edd.controller;
+
+public class MemberForm {
+    private String name;
+    //커맨드 + N
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
