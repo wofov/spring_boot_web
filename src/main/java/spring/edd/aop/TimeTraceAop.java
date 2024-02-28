19089bc53e613ba8f81300b8e3bf55ca6b5355ee
wofov <hys950526@naver.com>
2024-02-28 12:29:18 +0900

    aop 적용

---
+++ src/main/java/spring/edd/aop/TimeTraceAop.java
@@ -0,0 +1,26 @@
+package spring.edd.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component//빈 등록 위해
+public class TimeTraceAop {
+
+    @Around("execution(* spring.edd..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs +"ms");
+        }
+    }
+
+}
